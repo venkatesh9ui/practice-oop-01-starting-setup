1245cb77591d4c090db36123d16f6cc0c2eae2c2
Venkatesh <venkatesh9ui@gmail.com>
2021-10-18 10:51:45 -0400

    Setting Timers & Intervals by using setTimeout & setInterval

--- assets/scripts/analytics.js
+++ assets/scripts/analytics.js
@@ -1,1 +1,9 @@
-console.log('analytics sending...');
+// console.log('analytics sending...');
+
+const intervalId = setInterval(() => {
+  console.log('Sending analytics data...');
+}, 2000);
+
+document.getElementById('stop-analytics-btn').addEventListener('click', () => {
+  clearInterval(intervalId);
+});
